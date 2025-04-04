9d91a1112c9a4752a8270c2f36d51d0a75170f65
saul201287 <84940926+saul201287@users.noreply.github.com>
2025-04-04 09:51:00 -0600

    puto

--- src/horario/infraestructure/http/routes/Routes.go
+++ src/horario/infraestructure/http/routes/Routes.go
@@ -2,18 +2,18 @@ package routes
 
 import (
 	"api-v1/src/horario/infraestructure/http"
-	"api-v1/src/shared/middlewares"
+	//"api-v1/src/shared/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func HorarioRoutes(router *gin.RouterGroup){
-	middlewareAuth := middlewares.JWTAuthMiddleware()
+	//middlewareAuth := middlewares.JWTAuthMiddleware()
 	getAllController := http.SetUpGetAllController()
 	getByIdController := http.SetUpDeleteController()
 	createController := http.SetUpCreateController()
 
-	router.Use(middlewareAuth)
+	//router.Use(middlewareAuth)
 	router.POST("/", createController.Run)
 	router.GET("/", getAllController.Run)
 	router.DELETE("/:id", getByIdController.Run)
